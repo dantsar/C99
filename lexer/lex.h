5628437852e23423abcf5e4a6b6d75dd21752af1
tsar-daniel <danieltsarev@gmail.com>
2021-02-21 16:44:31 -0500

    lexer now recognizes character escape sequence

---
+++ lexer/lex.h
@@ -0,0 +1,33 @@
+#ifndef __LEX_H
+#define __LEX_H
+
+#include "tokens-manual.h"
+
+void set_yylval_int(unsigned long long num, int type, int sign);
+
+enum N_SIGN {N_UNSIGNED=0, N_SIGNED};
+enum N_TYPE {N_INT=0, N_LONG, N_LLONG, N_FLOAT, N_DOUBLE, N_LDOUBLE};
+
+struct Num{
+    unsigned long long int_num;
+    long double real;
+    int type;
+    int sign;
+};
+
+struct Str{
+    char *str;
+    int len;
+};
+
+typedef union{
+    struct Str str;
+    struct Num num;
+    char* ident;
+    char charlit;
+} YYSTYPE;
+
+YYSTYPE yylval;
+
+
+#endif
---
+++ lexer/prog.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    int num = 0;
+    num++;
+    printf("num %d\n", num);
+    return 0;
+}
